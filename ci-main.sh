c8398d01123d2fe8177e5b0f47ef90653706adfe
Jeremy Busk <jeremybusk@gmail.com>
2021-09-23 21:06:10 -0600

    use hosted runner/cleanup

--- ci-main.sh
+++ ci-main.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
+release=latest
+tag=uvoo/gitrepo-todir-sync:$release
 echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_USERNAME --password-stdin
-./buildPush.sh
+sudo docker build --tag $tag .
+sudo docker push $tag 
+docker logout
